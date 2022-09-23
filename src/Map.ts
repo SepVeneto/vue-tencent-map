f50a541f77c9704652719bef390f19a5e4390fd7
SepVeneto <sepveneto@qq.com>
2022-09-23 23:20:51 +0800

    feat: descriptor

--- src/Map.ts
+++ src/Map.ts
@@ -2,18 +2,16 @@ import { useMap } from "./hook"
 export class TencentMap {
   public map: any
   private callback: Function[] = []
-  private isLoad = false
+  private _isLoad = false
   constructor(dom: string) {
     useMap(dom).then((map) => {
       this.map = map
-      this.isLoad = true
+      this._isLoad = true
       this.callback.forEach(cb => cb())
     })
   }
+  @execute()
   setMarker(img: string, width: number, height: number) {
-    this.execute(() => this._setMarker(img, width, height))
-  }
-  _setMarker(img: string, width: number, height: number) {
     const scaleSize = new qq.maps.Size(width, height)
     const anchor = new qq.maps.Point(6, 6);
     const origin = new qq.maps.Point(0.1, 0.1);
@@ -24,11 +22,25 @@ export class TencentMap {
       position: this.map.getCenter()});
     return marker
   }
-  execute(cb: any) {
-    if (this.isLoad) {
-      cb()
-    } else {
-      this.callback.push(cb)
+  // execute(cb: any) {
+  //   if (this.isLoad) {
+  //     cb()
+  //   } else {
+  //     this.callback.push(cb)
+  //   }
+  // }
+}
+
+function execute() {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const fn = descriptor.value
+    descriptor.value = function (this: any, ...args: any[]) {
+      if (!this._isLoad) {
+        this.callback.push(() => fn.apply(this, args))
+      } else {
+        return fn.apply(this, args)
+      }
     }
+    return descriptor
   }
 }
